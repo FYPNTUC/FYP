fde4410dcf963ff7e6899e6fa749ec60065fc107
junhwee <jhlau96@hotmail.com>
2015-03-18 09:40:55 +0800

    Testing

---
+++ FYP/Assets/Scripts/LightningFlash.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LightningFlash : MonoBehaviour
+{
+	public Light myLight;
+	int Mtimer = 50;
+	public int MFlashtimer =2;
+	int counter = 0;
+	int Fcounter =0;
+
+	//Time myTime;
+	// Use this for initialization
+	void Start () 
+	{
+		myLight.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		counter ++;
+		Fcounter++;
+		if (counter <= Mtimer) 
+		{ 
+			if (Fcounter >= MFlashtimer)
+			{
+				myLight.enabled = !myLight.enabled;
+				Fcounter = 0;
+			}
+		}
+		counter++;
+
+	}
+}
