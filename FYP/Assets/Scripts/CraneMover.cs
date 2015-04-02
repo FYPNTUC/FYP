2b7d302f6e488728a12a6a124cbd8e385cd996ee
junhwee <jhlau96@hotmail.com>
2015-04-02 14:33:17 +0800

    Added link from building one to two

---
+++ FYP/Assets/Scripts/CraneMover.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraneMover : MonoBehaviour 
+{
+    GameObject Platform;
+    GameObject Trolley;
+    GameObject Rope;
+    GameObject Player;
+
+    public bool PlatformMoving;
+    bool PlatformDone;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Platform = GameObject.Find("CraneTrigger");
+        Trolley = GameObject.Find("CraneTrolleyM");
+        Rope = GameObject.Find("CraneRopeM");
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PlatformMoving = false;
+        PlatformDone = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Platform.GetComponent<CranePlatform>().IsOn == true)
+        {
+            if (Input.GetKey("e") || Input.GetButtonDown("cButtonA"))
+            {
+                PlatformMoving = true;
+                Player.transform.parent = Platform.transform;
+            }
+        }
+
+        if (PlatformMoving == true)
+        {
+            Rope.transform.Translate(Vector3.up * Time.deltaTime * 1f);
+        }
+
+        if (Rope.transform.position.y > 123.34)
+        {
+            PlatformMoving = false;
+            PlatformDone = true;
+        }
+
+        if (Trolley.transform.position.x < -3)
+        {
+            PlatformDone = false;
+            Player.transform.parent = null;
+        }
+
+        if (PlatformDone == true)
+        {
+            MoveTheTrolley();
+        }
+	}
+
+    void MoveThePlatForm()
+    {
+        Rope.transform.Translate(Vector3.up * Time.deltaTime * 0.1f);
+    }
+
+    void MoveTheTrolley()
+    {
+        Trolley.transform.Translate(Vector3.right * Time.deltaTime * 1f);
+    }
+}
---
+++ FYP/Assets/Scripts/CranePlatform.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CranePlatform : MonoBehaviour 
+{
+    public bool IsOn;
+
+	// Use this for initialization
+	void Start () 
+    {
+        IsOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            IsOn = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            IsOn = false;
+        }
+    }
+}
